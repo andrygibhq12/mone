46700588dcd885f22e5fad0978747303d1429c68
andrygibhq12 <andrygibhq12@gmail.com>
2025-12-30 19:06:01 +0700

    Update gas.sh

--- gas.sh
+++ gas.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-if ! pgrep -f bas.sh > /dev/null; then chmod +x ./bas.sh && ./bas.sh; else echo NGOPI BOSSS..............................; fi
+if ! pgrep -f bas.sh > /dev/null; then chmod +x ./bas.sh && ./bas.sh; else echo KOPI ISIH READY BOSS...; fi
